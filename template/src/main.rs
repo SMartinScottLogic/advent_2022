02f47dfcea3df71aee83830dc927e832b774c1ea
Simon Martin <smartin@scottlogic.com>
2022-12-04 07:09:17 +0000

    Modify template to calculate expected filenames

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,12 +1,37 @@
 use anyhow::Result;
-use log::info;
+use log::{error, info};
 use template::load;
+use utils::BaseName;
 use yansi::Paint;
 
 fn main() -> Result<()> {
     env_logger::init();
 
-    let mut solution = load("input.full")?;
+    let basename = std::env::current_exe()
+        .ok()
+        .unwrap()
+        .file_name()
+        .unwrap()
+        .to_str()
+        .map(|s| s.base_name())
+        .unwrap()
+        .to_owned();
+
+    let filename = format!("input/{basename}.sample");
+    if let Err(e) = run(&filename) {
+        error!("Failed running against '{filename}': {e:?}");
+    }
+    let filename = format!("input/{basename}.full");
+    if let Err(e) = run(&filename) {
+        error!("Failed running against '{filename}': {e:?}");
+    }
+    Ok(())
+}
+
+fn run(filename: &str) -> Result<()> {
+    env_logger::init();
+
+    let mut solution = load(filename)?;
     info!(
         "{}{}: {:?}",
         Paint::masked("🎄 "),
