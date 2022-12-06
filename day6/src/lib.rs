0fbbfbdd098d49eea19491bf6efdbcb27cfa9115
Simon Martin <smartin@scottlogic.com>
2022-12-06 05:31:51 +0000

    Day 6 complete

--- day6/src/lib.rs
+++ day6/src/lib.rs
@@ -1,20 +1,49 @@
-use std::io::{BufRead, BufReader};
+use std::{
+    collections::{HashSet, VecDeque},
+    io::{BufRead, BufReader},
+};
+
+use anyhow::anyhow;
 
 #[derive(Debug)]
-pub struct Solution {}
+pub struct Solution {
+    input: String,
+}
 
 impl utils::Solution for Solution {
-    type Result = anyhow::Result<u64>;
+    type Result = anyhow::Result<usize>;
     fn analyse(&mut self) {}
 
     fn answer_part1(&self) -> Self::Result {
-        // Implement for problem
-        Ok(0)
+        self.detect_distinct_window(4)
+            .ok_or_else(|| anyhow!("nothing"))
     }
 
     fn answer_part2(&self) -> Self::Result {
-        // Implement for problem
-        Ok(0)
+        self.detect_distinct_window(14)
+            .ok_or_else(|| anyhow!("nothing"))
+    }
+}
+
+impl Solution {
+    fn detect_distinct_window(&self, window_len: usize) -> Option<usize> {
+        let mut answer = None;
+        let mut window = VecDeque::new();
+        for (idx, c) in self.input.chars().enumerate() {
+            window.push_back(c);
+            if window.len() > window_len {
+                window.pop_front();
+            }
+            let chars = window.iter().fold(HashSet::new(), |mut acc, v| {
+                acc.insert(v);
+                acc
+            });
+            if chars.len() == window_len && answer.is_none() {
+                answer = Some(idx + 1);
+                break;
+            }
+        }
+        answer
     }
 }
 
@@ -22,9 +51,11 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
     type Error = std::io::Error;
 
     fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
-        let solution = Self {};
-        for _line in reader.lines() {
-            // Implement for problem
+        let mut solution = Self {
+            input: "".to_string(),
+        };
+        for line in reader.lines() {
+            solution.input = line?;
         }
         Ok(solution)
     }
