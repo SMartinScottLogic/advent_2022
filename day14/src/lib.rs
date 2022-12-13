a5980adfa04dc51ff949bbb9db1ff175fa3259db
Simon Martin <smartin@scottlogic.com>
2022-12-13 06:45:00 +0000

    Day 14 cloned from template

---
+++ day14/src/lib.rs
@@ -0,0 +1,33 @@
+use std::io::{BufRead, BufReader};
+
+type ResultType = u64;
+
+#[derive(Debug)]
+pub struct Solution {}
+
+impl utils::Solution for Solution {
+    type Result = anyhow::Result<ResultType>;
+    fn analyse(&mut self) {}
+
+    fn answer_part1(&self) -> Self::Result {
+        // Implement for problem
+        Ok(0)
+    }
+
+    fn answer_part2(&self) -> Self::Result {
+        // Implement for problem
+        Ok(0)
+    }
+}
+
+impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
+    type Error = std::io::Error;
+
+    fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
+        let solution = Self {};
+        for _line in reader.lines() {
+            // Implement for problem
+        }
+        Ok(solution)
+    }
+}
