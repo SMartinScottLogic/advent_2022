bb5a891c169ffe612ae00d2b060241421307c059
Simon Martin <smartin@scottlogic.com>
2022-12-16 19:51:31 +0000

    Day 16 complete
    Sample data doesn't work for part2

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -4,5 +4,5 @@ use day16::{ResultType, Solution};
 fn main() -> Result<()> {
     env_logger::init();
 
-    utils::run::<Solution, ResultType>(&["sample"], &[])
+    utils::run::<Solution, ResultType>(&["sample"], &["full"])
 }
