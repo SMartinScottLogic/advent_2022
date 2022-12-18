205cf6e3ba69081daf3e78832548c613439b1f98
Simon Martin <smartin@scottlogic.com>
2022-12-18 08:27:19 +0000

    Day 18 (#31)
    * Day 18 cloned from template
    
    * Day 18 complete

---
+++ day18/src/main.rs
@@ -0,0 +1,8 @@
+use anyhow::Result;
+use day18::{ResultType, Solution};
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    utils::run::<Solution, ResultType>(&["sample", "sample2"], &["full"])
+}
